71b4fc6c87924ba237e5557a85307e9bf1c6e9a5
Mikfri <111983669+Mikfri@users.noreply.github.com>
2022-11-10 11:15:50 +0100

    Add files via upload

---
+++ Exceptions/Exceptions/FootballPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class FootballPlayer
+    {   /// <summary>
+        /// 1) Vi har nogle restrictioner:
+        /// Id:         int         id > 0
+        /// Name:       string      Minimum 2 characters
+        /// 2) Få bruget "Pæren" ude i venstre side for syntax hjælp (hover over den del du vil arbejde med for forskellige muligheder)
+        /// </summary>
+        private int _shirtNumber;
+        private string _name;
+
+        public int Id { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 2) throw new ArgumentException("Name: Has to be between 2 - 20 characters");
+                _name = value;
+            }
+        }
+
+        public int Age { get; set; }
+        public int ShirtNumber
+        {
+            get { return _shirtNumber; }
+            set
+            {
+                if (value < 1 || value > 99) throw new ArgumentOutOfRangeException("Shirt#: Range 1-99");
+                _shirtNumber = value;
+            }
+        }
+
+
+        public FootballPlayer(int id, string name, int age, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            ShirtNumber = shirtNumber;
+        }
+
+        /// <summary>
+        /// På et tidspunkt skal objecter gemmes i en fil og åbnes fra en fil.
+        /// Når man gemmer obj gemmes det som string - ikke objekter ("String" er en class)
+        /// De skal laves om til objecter!
+        /// Denne tomme constructor sørger for 
+        /// </summary>
+        public FootballPlayer()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Name)}={Name}, {nameof(Age)}={Age.ToString()}, {nameof(ShirtNumber)}={ShirtNumber.ToString()}}}";
+        }
+
+
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,45 @@
+﻿using Exceptions;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 1)Using bugg, Husk at tilføj Using <Filnavn>         2)Constructor bugg
+
+//String        < Er en class! 
+
+//FootballPlayer player01 = new FootballPlayer() { Id = 1, Name = "Mikkel", Age = 37, ShirtNumber = 8 }; // bruges i forbindelse med filer og databaser
+//FootballPlayer player02 = new FootballPlayer(2, "Henrik", 50, 3); // bruges i forbindelse med alm oprettelse/initialisering
+
+FootballPlayer player01 = new FootballPlayer();
+FootballPlayer player02 = new FootballPlayer();
+
+
+///Try block
+///
+try
+{
+    player01 = new FootballPlayer() { Id = 1, Name = "Mikkel", Age = 37, ShirtNumber = 8 };
+    player02 = new FootballPlayer(2, "P", 34, 3);
+}
+///Når man bruger Exceptions skal den mest specialiserede/eller den længst nede i hierakiet:
+///Stå i toppen/bruges først af catch metoderne - ellers virker det ikke.
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine("Ex:1: " + ex.Message);
+}
+///Catch block
+///
+catch (ArgumentException ex)
+{
+    player01 = new FootballPlayer();
+    Console.WriteLine("EX:2 " + ex.Message);
+
+}
+
+
+Console.WriteLine();
+//Console.WriteLine(player02.Name.Length);
